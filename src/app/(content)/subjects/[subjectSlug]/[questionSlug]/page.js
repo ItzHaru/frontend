cc9a3891b40c69c5a47b7728a67809cd67c5f2a4
ItzHaru <ruzena05perinova@gmail.com>
2024-01-13 22:11:24 +0100

    test

--- src/app/(content)/subjects/[subjectSlug]/[questionSlug]/page.js
+++ src/app/(content)/subjects/[subjectSlug]/[questionSlug]/page.js
@@ -1,42 +1,46 @@
-import Content from "./Content";
-import { getClient } from "@/lib/client";
-import { gql } from "@apollo/client";
+// import Content from "./Content";
+// import { getClient } from "@/lib/client";
+// import { gql } from "@apollo/client";
 
-const getSubject = gql`
-  query {
-    subjects {
-      data {
-        attributes {
-          Slug
-          questions {
-            data {
-              attributes {
-                Slug
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// const getSubject = gql`
+//   query {
+//     subjects {
+//       data {
+//         attributes {
+//           Slug
+//           questions {
+//             data {
+//               attributes {
+//                 Slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export const dynamicParams = "blocking";
+// export const dynamicParams = "blocking";
 
-export async function generateStaticParams() {
-  const client = getClient();
-  const { data } = await client.query({ query: getSubject });
-  const result = data.subjects.data.map((subject) => {
-    return subject.attributes.questions.data.map((question) => {
-      return {
-        subjectSlug: subject.attributes.Slug,
-        questionSlug: question.attributes.Slug,
-      };
-    });
-  });
-  return result.flat();
-}
+// export async function generateStaticParams() {
+//   const client = getClient();
+//   const { data } = await client.query({ query: getSubject });
+//   const result = data.subjects.data.map((subject) => {
+//     return subject.attributes.questions.data.map((question) => {
+//       return {
+//         subjectSlug: subject.attributes.Slug,
+//         questionSlug: question.attributes.Slug,
+//       };
+//     });
+//   });
+//   return result.flat();
+// }
+
+// export default function Page({ params }) {
+//   return <Content params={params} />;
+// }
 
 export default function Page({ params }) {
-  return <Content params={params} />;
+  return <>beasca</>;
 }
